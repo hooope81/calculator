49e8e127a839e7dbcf3968dcddca50ca3ba2f6cd
hooope81 <hooope81@gmail.com>
2022-11-15 23:41:08 +0300

    js2

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 let buttons = [...document.querySelectorAll('.action')];
 let input = document.querySelector('input');
 let equality = document.querySelector('.equality');
-let numb1 = 0;
-let numb2 = 0;
+let numb = 0;
 let action = '';
+let result = 0;
 
 function getAction(numb1, numb2, action) {
     switch (action) {
@@ -18,18 +18,28 @@ function getAction(numb1, numb2, action) {
 }
 
 function getResult() {
-    numb2 = +input.value;
-    input.value = getAction(numb1, numb2, action);
+    numb = +input.value;
+    result = getAction(result, numb, action);
+    input.value = result;
 }
 
 buttons.forEach(item => {
     item.addEventListener('click', () => {
-        numb1 = +input.value;
-        action = item['name'];
-        input.value = '';
-        console.log(numb1);
-        console.log(action);
+        if(!result) {
+            result = +input.value;
+            action = item['name'];
+            input.value = '';
+        } else {
+            numb = +input.value;
+            result = getAction(result, numb, action);
+            action = item['name'];
+            input.value = '';
+        }
+
     })
 })
 
-equality.addEventListener('click', getResult);
+equality.addEventListener('click',() => {
+    getResult();
+    result = 0;
+});
