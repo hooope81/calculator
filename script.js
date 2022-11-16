96f7195d847792ccaa8ddde13f88467793603bc3
hooope81 <hooope81@gmail.com>
2022-11-16 23:01:15 +0300

    js3

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function getAction(numb1, numb2, action) {
         case "difference": return numb1 - numb2;
         case "multiplication": return numb1 * numb2;
         case "division": return numb1 / numb2;
+
         default: return  "error"
     }
 }
@@ -25,21 +26,51 @@ function getResult() {
 
 buttons.forEach(item => {
     item.addEventListener('click', () => {
-        if(!result) {
-            result = +input.value;
-            action = item['name'];
-            input.value = '';
+
+        if(item['name'] === "changeSign") {
+            if (!result) {
+                result = -input.value;
+            }
+            numb = -input.value;
+            input.value = numb;
+
+        } else if(item['name'] === "clean") {
+            result = 0;
+            numb = 0;
+            input.value = '0';
+
+        } else if(item['name'] === "percent") {
+            if (!result) {
+               result = +input.value * 100 / result;
+            }
+            numb = +input.value * 100 / result;
+            input.value = numb;
+
         } else {
-            numb = +input.value;
-            result = getAction(result, numb, action);
-            action = item['name'];
-            input.value = '';
+            if(!result) {
+                result = +input.value;
+                action = item['name'];
+                input.value = result;
+
+            } else {
+                numb = +input.value;
+                result = getAction(result, numb, action);
+                action = item['name'];
+                input.value = result;
+            }
         }
 
+
+
+
+        console.log(result);
+        // console.log(numb);
     })
 })
 
 equality.addEventListener('click',() => {
     getResult();
     result = 0;
+    console.log(result);
+    // console.log(numb);
 });
