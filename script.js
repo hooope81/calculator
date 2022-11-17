567f1eb4adad286b47c68d235345dc0cbcc799b3
hooope81 <hooope81@gmail.com>
2022-11-17 22:30:17 +0300

    js4

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 let buttons = [...document.querySelectorAll('.action')];
 let input = document.querySelector('input');
 let equality = document.querySelector('.equality');
+let digits = [...document.querySelectorAll('.numb')];
 let numb = 0;
 let action = '';
 let result = 0;
+let changeSign = false;
 
 function getAction(numb1, numb2, action) {
     switch (action) {
@@ -14,7 +16,6 @@ function getAction(numb1, numb2, action) {
         case "multiplication": return numb1 * numb2;
         case "division": return numb1 / numb2;
 
-        default: return  "error"
     }
 }
 
@@ -22,17 +23,25 @@ function getResult() {
     numb = +input.value;
     result = getAction(result, numb, action);
     input.value = result;
+    console.log(result);
 }
 
 buttons.forEach(item => {
     item.addEventListener('click', () => {
 
+
+
         if(item['name'] === "changeSign") {
             if (!result) {
                 result = -input.value;
+                input.value = result;
+                changeSign = true;
+                console.log(numb, result,action);
+            } else {
+                numb = -input.value;
+                input.value = numb;
             }
-            numb = -input.value;
-            input.value = numb;
+
 
         } else if(item['name'] === "clean") {
             result = 0;
@@ -53,18 +62,21 @@ buttons.forEach(item => {
                 input.value = result;
 
             } else {
-                numb = +input.value;
-                result = getAction(result, numb, action);
-                action = item['name'];
-                input.value = result;
+                if(changeSign){
+                    action = item['name'];
+                } else {
+                    numb = +input.value;
+                    result = getAction(result, numb, action);
+                    action = item['name'];
+                    input.value = result;
+                }
+
+
             }
+            input.value = '';
         }
-
-
-
-
         console.log(result);
-        // console.log(numb);
+
     })
 })
 
@@ -74,3 +86,13 @@ equality.addEventListener('click',() => {
     console.log(result);
     // console.log(numb);
 });
+
+digits.forEach(item => {
+    item.addEventListener('click', () => {
+       if(input.value === '0') {
+            input.value = item.textContent;
+        } else {
+            input.value += item.textContent;
+        }
+    })
+})
